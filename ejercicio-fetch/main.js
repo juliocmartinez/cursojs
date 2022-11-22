a59e1020ddd17c02e21c18a0cdbaa954d8669b14
Julio Cesar Martinez Garcia <julio.martinezg8@gmail.com>
2022-11-21 23:34:52 -0500

    fetch terminado

--- ejercicio-fetch/main.js
+++ ejercicio-fetch/main.js
@@ -10,29 +10,32 @@ getDatosNasa()
         let datosNasa = datos
 
         let container = document.querySelector("#container")
+        container.style.width="95%"
+        container.style.margin="0px auto"
 
-        let arrayDatos = Object.values(datosNasa)
-        console.log(arrayDatos)
+        container.style.textAlign="justify"
 
-        arrayDatos.map((dato) => {
-            let item = document.createElement("div")
+        // let arrayDatos = Object.values(datosNasa)
+        // console.log(arrayDatos)
+        let title = document.createElement("h1")
+        let img = document.createElement ("img")
+        let description = document.createElement ("p")
+        let count = document.createElement("a")
+        count.style.fontSize="10px"
 
-            item.append(dato)
-            container.append(item)
-            container.style.display = "flex"
-            container.style.flexWrap = "wrap"
-            container.style.background = "#ccc"
-            item.style.border = "1px solid black"
-            item.style.width = "150px"
-            item.style.height="150px"
-            item.style.margin = "10px"
-            item.style.fontSize = "17px"
-            item.style.overflow = "hidden"
+        title.append(datosNasa.title)
+        img.src=datosNasa.url  
+        description.append(datosNasa.explanation)
+        img.style.width="100%"
+        count.href=datosNasa.hdurl
+        count.target="_blank"
+        count.append(datosNasa.hdurl)
+        container.append(title)
+        container.append(img)
+        container.append(count)
+        container.append(description)
 
 
-
-
-        })
 
 
 
